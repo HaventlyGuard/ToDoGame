f7acc8d54aae143269501badc132e1870d3fbab7
Лямин Тимофей Александрович <ip316s15@csc.sibsutis.ru>
2025-06-25 02:02:50 +0700

    Основной вью модел для упр состояниями

---
+++ MVVW/ViewModel/MainViewModel.cs
@@ -0,0 +1,33 @@
+﻿using menuApp.Core;
+using menuApp.MVVW.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menuApp.MVVW.ViewModel
+{
+    internal class MainViewModel : ObserverObject
+    {
+        public HomeViewModel HomeVM { get; set; }
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { 
+                _currentView = value;
+                OnPropertyChanged();            
+            }
+        }
+
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+        }
+    }
+}
